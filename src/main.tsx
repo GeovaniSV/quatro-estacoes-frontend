55ca04a62c0f4d94eb91c0132d80d9f180df1cc0
Geovani Dos Santos Vargas <geovani.020204@gmail.com>
2026-01-31 09:13:14 -0400

    react router dom configuration

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,18 @@
+import "./index.css";
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import "./index.css";
-import Layout from "./Layout.tsx";
+import {
+  createBrowserRouter,
+  RouterProvider,
+  Navigate,
+} from "react-router-dom";
+
+const router = createBrowserRouter([
+  { path: "/", element: <Navigate to={"/login"} /> },
+]);
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <Layout />
+    <RouterProvider router={router} />
   </StrictMode>,
 );
